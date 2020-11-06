51b2e930a04c1f358923f6f35741a8d2f4bb765a
journeyAa <Aalia.Alper515@gmail.com>
2020-11-06 17:17:28 -0500

    other comments added under Aalia branch

--- src/test/java/HelloGit.java
+++ src/test/java/HelloGit.java
@@ -1,5 +1,7 @@
 public class HelloGit {
     public static void main(String[] args) {
+        // we are trything this merging but it is not really working seems like./...
+        // thank you Nigmat though
 
     }
 }
