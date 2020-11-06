9f7df91b04b823e7c1b947ec68fd2d2a003b0768
journeyAa <Aalia.Alper515@gmail.com>
2020-11-06 17:16:41 -0500

    comments added under Aalia branch

--- src/main/java/Branch1_Practice.java
+++ src/main/java/Branch1_Practice.java
@@ -1,6 +1,7 @@
 public class Branch1_Practice {
     public static void main(String[] args) {
         System.out.println("I did sth and I don't wanna whatever I will do mess up my master");
+        // I totally agree and this is getting messy, this is written by Aalia
 
         System.out.println("This is the shortcut");
         // This is real branch1
