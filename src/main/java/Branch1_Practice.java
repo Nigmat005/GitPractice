21fa589b486acd3ccca45ca4d8be2bfad19d12fc
Nigmat005 <34325489+Nigmat005@users.noreply.github.com>
2020-11-06 00:45:34 -0500

    Branch1

--- src/main/java/Branch1_Practice.java
+++ src/main/java/Branch1_Practice.java
@@ -3,5 +3,6 @@ public class Branch1_Practice {
         System.out.println("I did sth and I don't wanna whatever I will do mess up my master");
 
         System.out.println("This is the shortcut");
+        // This is real branch1
     }
 }
