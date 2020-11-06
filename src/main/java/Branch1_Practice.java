924bd81d5655a8faf1d565e670e0db5dad66b44a
Nigmat005 <34325489+Nigmat005@users.noreply.github.com>
2020-11-06 00:41:42 -0500

    Happening on the Branch1

---
+++ src/main/java/Branch1_Practice.java
@@ -0,0 +1,7 @@
+public class Branch1_Practice {
+    public static void main(String[] args) {
+        System.out.println("I did sth and I don't wanna whatever I will do mess up my master");
+
+        System.out.println("This is the shortcut");
+    }
+}
